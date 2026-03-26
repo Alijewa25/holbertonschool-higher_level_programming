fd27af338b591e4974902c45279e9808c0043e98
Alijewa25 <tahminaalijewa@gmail.com>
2026-03-26 16:11:37 +0400

    student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""izah"""
+"""
+Student klassı üçün modul izahı.
+"""
+
 
 class Student:
-    """izah"""
+    """Tələbəni təyin edən klass."""
 
     def __init__(self, first_name, last_name, age):
-        """izah"""
+        """Obyekt yaradılarkən atributları mənimsədir."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """izah"""
-        return self.__dict__
+        """Obyektin lüğət (dict) təmsilini qaytarır."""
+        return self.__dict__	
