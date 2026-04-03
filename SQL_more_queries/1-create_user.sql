92abe2908414dd65e3df1847d9343e5fc054e7d7
Alijewa25 <tahminaalijewa@gmail.com>
2026-04-03 07:18:26 +0000

    sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,10 @@
 --The user_0d_1 password should be set to user_0d_1_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd
+'
 --user_0d_1 should have all privileges on your MySQL server
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+WITH GRANT OPTION
+
+--Apply changes
+FLUSH PRIVILEGES
