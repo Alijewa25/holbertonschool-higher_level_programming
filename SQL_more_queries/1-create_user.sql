68cc3b7b7b128b82df2942dca5da4e8f7aa46154
Alijewa25 <tahminaalijewa@gmail.com>
2026-04-03 07:36:04 +0000

    sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
---izah
+-- Create the user if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
---izah
+
+-- Grant all privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
