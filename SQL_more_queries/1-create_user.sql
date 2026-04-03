594eebe30fc62c548a2027cd737fb6975773447b
Alijewa25 <tahminaalijewa@gmail.com>
2026-04-03 07:22:59 +0000

    sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,13 +1,3 @@
--- Remove user if it exists (prevents failure)
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-
--- Create user with password
-CREATE USER 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
-WITH GRANT OPTION;
-
--- Reload privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
