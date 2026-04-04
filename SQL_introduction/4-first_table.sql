844bb7dd062b26ac07f68037eadd5dba6ec48d6d
Alijewa25 <tahminaalijewa@gmail.com>
2026-04-04 11:51:44 +0400

    sql

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,2 @@
--- script that performs a specific task
--- script that performs a specific task
--- Create a TABLE named first_TABLE IF NOT EXISTS with one column id of type INT and a column name of type VARCHAR(256).
-CREATE TABLE IF NOT EXISTS first_TABLE (
-	id INT,
-	name VARCHAR(256)
-);
+-- Creates a table called first_table in the current MySQL server
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
