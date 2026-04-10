850e2e435689cccaf9797ca4ab107ba25971c690
Alijewa25 <tahminaalijewa@gmail.com>
2026-04-10 23:37:44 +0400

    p

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,23 +6,28 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    """izah"""
+    # Vacib: İndeksləri DƏQİQ belə yazmalısan
+    # sys.argv -> username (məs: root)
+    # sys.argv -> password (məs: root)
+    # sys.argv -> database (məs: hbtn_0e_0_usa)
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv,
-        passwd=sys.argv,
-        db=sys.argv
+        user=sys.argv,     # Burada olmalıdır
+        passwd=sys.argv,   # Burada olmalıdır (Səndə səhv buradadır)
+        db=sys.argv        # Burada olmalıdır
     )
 
     cursor = db.cursor()
-
+    
+    # İD-yə görə artan sırada sıralayırıq
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
+    
     rows = cursor.fetchall()
-
     for row in rows:
         print(row)
-
+    
+    # Bağlantıları təmiz bağlayırıq
     cursor.close()
     db.close()
