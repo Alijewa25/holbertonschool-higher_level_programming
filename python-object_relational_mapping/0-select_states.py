2ce1889cf732f665180a3bc5dc4c28661ed9d896
Alijewa25 <tahminaalijewa@gmail.com>
2026-04-10 23:46:48 +0400

    python

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,13 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa.
-"""
+"""Lists all states"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Vacib: İndeksləri DƏQİQ belə yazmalısan
-    # sys.argv -> username (məs: root)
-    # sys.argv -> password (məs: root)
-    # sys.argv -> database (məs: hbtn_0e_0_usa)
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv,     # Burada olmalıdır
-        passwd=sys.argv,   # Burada olmalıdır (Səndə səhv buradadır)
-        db=sys.argv        # Burada olmalıdır
-    )
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv, passwd=sys.argv, db=sys.argv)
     cursor = db.cursor()
-    
-    # İD-yə görə artan sırada sıralayırıq
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    
-    rows = cursor.fetchall()
-    for row in rows:
+    for row in cursor.fetchall():
         print(row)
-    
-    # Bağlantıları təmiz bağlayırıq
     cursor.close()
     db.close()
