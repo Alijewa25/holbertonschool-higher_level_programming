9392683e67ef0ea8d42a6dbe87f6e43486ca69bd
Alijewa25 <tahminaalijewa@gmail.com>
2026-05-02 10:37:31 +0400

    add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-""" Shebang """
+"""
+Bu modul iki float ədədi toplayan və nəticəni float olaraq 
+qaytaran tip annotasiyalı funksiyanı ehtiva edir.
+"""
 
-def add(a: float, b: float) -> float :
-    """izah"""
-    return(a+b)
+
+def add(a: float, b: float) -> float:
+    """
+    izah
+    """
+    return a + b
